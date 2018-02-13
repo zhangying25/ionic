5f9db05d9f07cd75eae09480ccbfa857f2fe3f45
Ying Zhang <zhang_ying@msn.com>
2018-02-13 15:15:51 -0800

    format

--- www/js/app.js
+++ www/js/app.js
@@ -13,7 +13,6 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -26,23 +25,23 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
   $stateProvider
 
     .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/sidebar.html',
-    controller: 'AppCtrl'
-  })
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/sidebar.html',
+      controller: 'AppCtrl'
+    })
 
-  .state('app.home', {
-    url: '/home',
-    views: {
-      'mainContent': {
-        templateUrl: 'templates/home.html',
-        controller: 'IndexController'
+    .state('app.home', {
+      url: '/home',
+      views: {
+        'mainContent': {
+          templateUrl: 'templates/home.html',
+          controller: 'IndexController'
+        }
       }
-    }
-  })
+    })
 
-  .state('app.aboutus', {
+    .state('app.aboutus', {
       url: '/aboutus',
       views: {
         'mainContent': {
@@ -50,36 +49,37 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
           controller: 'AboutController'
         }
       }
-  })
+    })
 
-  .state('app.contactus', {
-      url: '/contactus',
+    .state('app.contactus', {
+        url: '/contactus',
+        views: {
+          'mainContent': {
+            templateUrl: 'templates/contactus.html'
+          }
+        }
+    })
+
+    .state('app.menu', {
+        url: '/menu',
+        views: {
+          'mainContent': {
+            templateUrl: 'templates/menu.html',
+            controller: 'MenuController'
+          }
+        }
+    })
+
+    .state('app.dishdetails', {
+      url: '/menu/:id',
       views: {
         'mainContent': {
-          templateUrl: 'templates/contactus.html'
+          templateUrl: 'templates/dishdetail.html',
+          controller: 'DishDetailController'
         }
       }
-  })
-
-  .state('app.menu', {
-      url: '/menu',
-      views: {
-        'mainContent': {
-          templateUrl: 'templates/menu.html',
-          controller: 'MenuController'
-        }
-      }
-  })
-
-  .state('app.dishdetails', {
-    url: '/menu/:id',
-    views: {
-      'mainContent': {
-        templateUrl: 'templates/dishdetail.html',
-        controller: 'DishDetailController'
-      }
-    }
-  });
+    });
+    
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
 });
