0700020cc0ac13896240fa9a8fa4101981c58b62
Ying Zhang <zhang_ying@msn.com>
2018-02-16 14:36:11 -0800

    local storage

--- www/js/services.js
+++ www/js/services.js
@@ -22,6 +22,23 @@ angular.module('conFusion.services', ['ngResource'])
     return $resource(baseURL + "feedback/:id");
   }])
 
+  .factory('$localStorage', ['$window', function($window) {
+    return {
+      store: function(key, value) {
+        $window.localStorage[key] = value;
+      },
+      get: function(key, defaultValue) {
+        return $window.localStorage[key] || defaultValue;
+      },
+      storeObject: function(key, value) {
+        $window.localStorage[key] = JSON.stringify(value);
+      },
+      getObject: function(key,defaultValue) {
+        return JSON.parse($window.localStorage[key] || defaultValue);
+      }
+    }
+  }])
+
   .factory('favoriteFactory', ['$resource', 'baseURL', function($resource, baseURL) {
     var favFac = {};
     var favorites = [];
